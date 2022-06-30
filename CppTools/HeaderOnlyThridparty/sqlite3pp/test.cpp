abea38aaa7dce7221a90dda4021a8b9536b4e86a
zPhae <692841846@qq.com>
2022-07-01 00:11:45 +0800

    commit sqlite3pp

---
+++ CppTools/HeaderOnlyThridparty/sqlite3pp/test.cpp
@@ -0,0 +1,21 @@
+/*******************************************************************************
+ *FileName: test.cpp
+ *version:
+ *Author: Phae
+ *Date: 2022-06-30 20:30:10
+ *Description:
+ *LastEditors: Phae
+ *LastEditTime: 2022-06-30 23:53:11
+ ********************************************************************************/
+#include "dbg.hpp"
+#include "sqlite3pp.hpp"
+int main(int argc, char** argv) {
+    string path = "./test.db";
+    Sqlite3pp db(path.c_str());
+    db.db_select("user", "", "", "", "");
+    string cmd = "SELECT * FROM sqlite_master WHERE type='table' ";
+     db.db_sql(cmd);
+    db.db_create("test", "id,name");
+    db.db_insert("test", "id", "1");
+    return 0;
+}
