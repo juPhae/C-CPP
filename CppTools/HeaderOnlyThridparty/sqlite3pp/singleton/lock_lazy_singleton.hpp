2b760ad36a96ca278dcf3394416cac4e297215f5
speedster <227248480@qq.com>
2022-10-25 16:30:53 +0800

    update singleton

--- CppTools/HeaderOnlyThridparty/sqlite3pp/singleton/lock_lazy_singleton.hpp
+++ CppTools/HeaderOnlyThridparty/sqlite3pp/singleton/lock_lazy_singleton.hpp
@@ -48,45 +48,45 @@ public:
     }
   }
 
-  ////////////////////////// 接口部分 //////////////////////////
+  // ////////////////////////// 接口部分 //////////////////////////
 
-  string sql_cmd(string sql) {
-    std::string string_array = Sqlite3pp::db_sql(sql);
-    return string_array;
-  }
+  // string sql_cmd(string sql) {
+  //   std::string string_array = Sqlite3pp::db_sql(sql);
+  //   return string_array;
+  // }
 
-  string sql_drop(string table) {
-    std::string string_array = Sqlite3pp::db_drop(table);
-    return string_array;
-  }
+  // string sql_drop(string table) {
+  //   std::string string_array = Sqlite3pp::db_drop(table);
+  //   return string_array;
+  // }
 
-  string sql_delete(string table, string where) {
-    std::string string_array = Sqlite3pp::db_delete(table, where);
-    return string_array;
-  }
+  // string sql_delete(string table, string where) {
+  //   std::string string_array = Sqlite3pp::db_delete(table, where);
+  //   return string_array;
+  // }
 
-  string sql_create(string table, string col_para) {
-    std::string string_array = Sqlite3pp::db_create(table, col_para);
-    return string_array;
-  }
+  // string sql_create(string table, string col_para) {
+  //   std::string string_array = Sqlite3pp::db_create(table, col_para);
+  //   return string_array;
+  // }
 
-  string sql_update(string table, string set, string where) {
-    std::string string_array = Sqlite3pp::db_update(table, set, where);
-    return string_array;
-  }
+  // string sql_update(string table, string set, string where) {
+  //   std::string string_array = Sqlite3pp::db_update(table, set, where);
+  //   return string_array;
+  // }
 
-  string sql_insert(string table, string cols, string vals) {
-    std::string string_array = Sqlite3pp::db_insert(table, cols, vals);
-    return string_array;
-  }
+  // string sql_insert(string table, string cols, string vals) {
+  //   std::string string_array = Sqlite3pp::db_insert(table, cols, vals);
+  //   return string_array;
+  // }
 
-  string sql_select(string table, string colname, string where, string order,
-                    string limit) {
-    std::string string_array =
-        Sqlite3pp::db_select(table, colname, where, order, limit);
-    return string_array;
-  }
-  ////////////////////////// 接口部分 //////////////////////////
+  // string sql_select(string table, string colname, string where, string order,
+  //                   string limit) {
+  //   std::string string_array =
+  //       Sqlite3pp::db_select(table, colname, where, order, limit);
+  //   return string_array;
+  // }
+  // ////////////////////////// 接口部分 //////////////////////////
 
 private:
   // 将其构造和析构成为私有的, 禁止外部构造和析构
