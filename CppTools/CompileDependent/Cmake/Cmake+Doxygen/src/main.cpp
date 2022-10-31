f9acd9c757468336cd4aac4abf64f2ac00778687
speedster <227248480@qq.com>
2022-10-31 12:52:14 +0800

    build version

--- CppTools/CompileDependent/Cmake/Cmake+Doxygen/src/main.cpp
+++ CppTools/CompileDependent/Cmake/Cmake+Doxygen/src/main.cpp
@@ -1,12 +1,12 @@
 
-/******************************************************************************* 
-  *FileName: main.cpp
-  *version: 
-  *Author: LZH
-  *Date: 2022-06-19 10:55:29
-  *Description: 
-  *LastEditors: LZH
-  *LastEditTime: 2022-06-22 17:45:20
+/*******************************************************************************
+ *FileName: main.cpp
+ *version:
+ *Author: phae
+ *Date: 2022-06-19 10:55:29
+ *Description:
+ *LastEditors: phae
+ *LastEditTime: 2022-10-31 12:47:34
  ********************************************************************************/
 
 /**
@@ -21,8 +21,25 @@
  */
 
 #include "Message.hpp"
+#include "version.h"
 #include <iostream>
+#define VERSIONS                                                               \
+  VERSION_MAJOR "." VERSION_MINOR "." VERSION_PATCH " " BUILD_TIMESTAMP
+
+/*
+版本格式：主版本号.次版本号.修订号 时间戳
+
+版本号递增规则如下：
+      主版本号：当你做了不兼容的 API 修改，
+      次版本号：当你做了向下兼容的功能性新增，
+      修订号：当你做了向下兼容的问题修正。
+      先行版本号及版本编译元数据可以加到“主版本号.次版本号.修订号”的后面，作为延伸。
+
+*/
+
 int main() {
+  std::cout << "System V" << VERSIONS << std::endl;
+
   Message say_hello("Hello, CMake World!");
   std::cout << say_hello << std::endl;
   Message say_goodbye("Goodbye, CMake World");
