8b46ab275ecc09aeb1727bc2eddc9c62a323d3c6
speedster <227248480@qq.com>
2022-11-02 15:24:21 +0800

    update oat ++

--- CppTools/CompileDependent/oat++/oatppServer/scripts/make-doc.sh
+++ CppTools/CompileDependent/oat++/oatppServer/scripts/make-doc.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 # 检测nodejs
-NODEJS_V=$(node -v)
-NODEJS_V_
-echo "nodejs version : "${NODEJS_V}
-if [[ $NODEJS_V < "v16" ]]; then
+NODEJS_V=$(N_V=$(node -v) && echo ${N_V:1})
+echo "node version : "${NODEJS_V}
+if [[ $NODEJS_V < "16" ]]; then
+    echo "The version is too low.Reset nodejs's version"
     # passwd需要替换成正确的密码
     echo passwd | sudo -S n 16
-    NODEJS_V_=$(node -v)
+    CHANGE_NODE_V=true
+    NODEJS_V_=$(N_V=$(node -v) && echo ${N_V:1})
+
 fi
-echo "nodejs version : "${NODEJS_V_}
 #生成文档
 cd .. && apidoc -i src/ -o docs/apidoc/
-if [[ $NODEJS_V > "v9" ]]; then
+
+if [[ $CHANGE_NODE_V == "true" ]]; then
+    echo "Reset nodejs's version"
+
+    echo "nodejs version : "${NODEJS_V_}
     # passwd需要替换成正确的密码
-    echo passwd | sudo -S n 8
+    echo passwd | sudo -S n $NODEJS_V
 fi
